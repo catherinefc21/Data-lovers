8ec70175cf05882f9e00fbbd07fb7667dc21d074
DaniLaraT <daniela.alejandra66@gmail.com>
2023-05-09 16:00:26 -0400

    recoge valor de lista desplegable y lo entrega a JS

--- src/main.js
+++ src/main.js
@@ -18,9 +18,19 @@ movies.forEach(movie=>{
   contenedor.innerHTML+= `<section> <br><img src="  ${movie.poster} ">  <br>${movie.rt_score} ⭐ <br>  ${movie.title} </section>;`
 })
 
+//Seleccionar en lista desplegable productor
+const typeSelect= document.getElementById("typeProductors");
+typeSelect.addEventListener("change", () => {
+  const selectProductor= typeSelect.value;
+  console.log(selectProductor);
+})
 
-
-
+//Seleccionar el lista desplegable director
+const typeSelect2= document.getElementById("typeDirectors");
+typeSelect2.addEventListener("change", () => {
+  const selectDirector= typeSelect.value;
+  console.log(selectDirector);
+})
 
 console.log(example, data);
 
