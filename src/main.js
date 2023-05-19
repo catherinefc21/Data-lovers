821190d92aeb0d984f355c63f5ddd93ca536fc6a
catherinefc21 <129704819+catherinefc21@users.noreply.github.com>
2023-05-19 13:51:21 -0400

    funciones

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
-import { example } from './data.js';
+import { filterProducer, orderMovies, filterDirector, selectMovie } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-//obtener lista de películas
 
+//obtener lista de películas
 const movies= data.films;
+console.log(movies)
 
 //PÁGINA INICIO (Bienvenida)
 const contenedor= document.getElementById("contenedor");
 contenedor.innerHTML+= `<div class="inicio"> 
-<div class="welcome"><div class=""><h2>Bienvenido(a)<br></h2><h3>¡Revisa nuestro menú y descubre todo sobre tus películas favoritas!</h3><br><br></div><img class="inicioImg"src="img/portada 3.png" alt=""></div>
+<div class="welcome"><div class="textWelcome"><h2>Bienvenido(a)<br></h2><h3>¡Revisa nuestro menú y descubre todo sobre tus películas favoritas!</h3><br></div><br><img class="inicioImg"src="img/portada 3.png" alt=""></div>
 </div>`
 //BOTON HISTORIA
 const history= document.getElementById("history");
 history.addEventListener("click", function(){
 contenedor.innerHTML=""
-  contenedor.innerHTML+= `<div class="history"><div class="historyImg"><img src="/src/img/history.png" alt=""></div><div class="history1"><h2>GHIBLI STUDIOS</h2><p> <br> <br> For anime fans, there is nothing like the works of Studio Ghibli. This small team of animators spends years crafting each wonderful story, bringing tales of adventure, love and friendship to life through thousands of hand-drawn frames. From My Neighbor Totoro to Ponyo, each of these enchanting films allows viewers to immerse themselves in fantasy worlds.
+contenedor.innerHTML+= `<div class="history"><div class="historyImg"><img src="/src/img/history.png" alt=""></div><div class="history1"><h2>GHIBLI STUDIOS</h2><p> <br> <br> For anime fans, there is nothing like the works of Studio Ghibli. This small team of animators spends years crafting each wonderful story, bringing tales of adventure, love and friendship to life through thousands of hand-drawn frames. From My Neighbor Totoro to Ponyo, each of these enchanting films allows viewers to immerse themselves in fantasy worlds.
   <br> <br>Studio Ghibli was founded by animator, director, producer, screenwriter, author and manga artist Hayao Miyazaki; Japanese film director Isao Takahata; and producer Toshio Suzuki. Over the past 37 years, they have created 20 feature films (and counting), plus several short films and television commercials. Read on to learn more about the history and future of this legendary animation studio. <br> <br></p></div></div>`
 })
 //BOTON VER TODAS LAS PELÍCULAS(VIEW ALL)
@@ -56,28 +56,24 @@ console.log(contenedor);
 const typeSelect= document.getElementById("typeProductors");
 typeSelect.addEventListener("change", () => {
  const selectProductor= typeSelect.value;
- //filtra el productor seleccionado
- const filterProducer= movies.filter((productor)=> {
-  return productor.producer === selectProductor;
-})//se deja contenedor en blanco y se muestras las películas del productor seleccionado
-  contenedor.innerHTML="";
-  filterProducer.forEach((peli)=>{
-    contenedor.innerHTML += `<section> <br><img src="  ${peli.poster} ">  <br>${peli.rt_score} ⭐ <br>  ${peli.title} </section>;`
-  });
+ 
+  const filtroProductor= filterProducer(movies,selectProductor);
+  console.log(filtroProductor)
+  contenedor.innerHTML="",
+  filtroProductor.forEach((peli)=>{
+  contenedor.innerHTML += `<section> <br><img src="${peli.poster}">  <br>${peli.rt_score} ⭐ <br>  ${peli.title} </section>`
+  })
 });
 
 //BOTON EN MENÚ PARA SELECCIONAR DIRECTOR
 const typeSelect2= document.getElementById("typeDirectors");
 typeSelect2.addEventListener("change", () => {
   const selectDirector= typeSelect2.value;
-  console.log(selectDirector);
   //FUNCION filtra el director seleccionado
-  const filterDirector= movies.filter((director)=> {
-    return director.director === selectDirector;
-  })//Lo que se tiene que mostrar en pantalla
-    contenedor.innerHTML="";
-    filterDirector.forEach((peli2)=>{
-      contenedor.innerHTML += `<section> <br><img src="  ${peli2.poster} ">  <br>${peli2.rt_score} ⭐ <br>  ${peli2.title} </section>;`
+  const filtroDirector= filterDirector(movies, selectDirector);
+  contenedor.innerHTML="";
+  filtroDirector.forEach((peli2)=>{
+  contenedor.innerHTML += `<section> <br><img src="  ${peli2.poster} ">  <br>${peli2.rt_score} ⭐ <br>  ${peli2.title} </section>`
     });
   });
 
@@ -89,38 +85,30 @@ movies.forEach(list=>{
 //Seleccionar pelicula para ver personajes
 const typeSelect3= document.getElementById("listMovies");
 typeSelect3.addEventListener("change", () => {
-
-  //Funcion filtrar personajes segun movie. 
-  const selectMovie= movies.find(pelicula=> pelicula.title===typeSelect3.value);
-  if (selectMovie){
-
+ const selectFilms= typeSelect3.value;
+ const seleccionarMovie= selectMovie(movies, selectFilms);
+ console.log(seleccionarMovie);
     //contenedor con div id slider para contener carrusel
     contenedor.innerHTML=`<div id="slider"></div>`;
-    selectMovie.people.forEach((personaje)=>{
+    seleccionarMovie.people.forEach((personaje)=>{
       document.getElementById("slider").innerHTML += `<div class= "character"> <br><div class=imgPeople><img src="${personaje.img}"></div> <br><div class= information><h3>${personaje.name}</h3> <b>Age:</b> ${personaje.age} <br> <b>Gender:</b> ${personaje.gender} <br> <b>Specie:</b> ${personaje.specie} <br> <b>Eye color:</b> ${personaje.eye_color}<br> <b>Hair color:</b>${personaje.hair_color}</div> </div>`
-    }
-    )
-  }
-})
+    })}
+);
+
   //BOTON ORDENAR A-Z(clonar movies para usar esa constant y ordenarla)
   movies.itself = movies 
   const cloneMovies = structuredClone(movies);
-
   const order= document.getElementById("order");
-  order.addEventListener("click", function(){
-  const orderMovies= cloneMovies.sort(function(a, b){
-  if (a.title>b.title){
-    return 1;
-  }if (a.title<b.title){
-    return -1;
-  }
-    return 0;
-})
+
+  const ordenarPeliculas= orderMovies(cloneMovies);
+  console.log(ordenarPeliculas);
+  order.addEventListener("click", ()=>{ordenarPeliculas,
+
 //contenedor vacío. Aplicar función de ordenado para que se muestren las películas en el contenedor.
-  contenedor.innerHTML="";
-  orderMovies.forEach((order)=>{
+  contenedor.innerHTML="",
+  ordenarPeliculas.forEach((order)=>{
     contenedor.innerHTML +=`<section> <br><img src="  ${order.poster} ">  <br>${order.rt_score} ⭐ <br>  ${order.title} </section>`
   })
-})
+});
 
-console.log(example, data);
+
